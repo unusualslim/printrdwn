c31d8b54989b1685fcb1a8619a7f32f0080d9831
unusualslim <lance@perrybrothersoil.com>
2020-09-19 08:23:12 -0400

    rails6

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root :to => "cases#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => "cases#index"
 
   get "pages/:page" => "pages#show"   
   devise_for :users, controllers: {
