298dd4aa0d19097d0d7533db2c4f5f84d302e568
unusualslim <lance@perrybrothersoil.com>
2020-09-21 17:37:44 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root :to => "cases#index"
+  root :to => "pages#index"
 
   devise_for :users
 
