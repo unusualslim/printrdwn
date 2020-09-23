32539839f9f36c639d0425c4c467ee7aec369d2c
unusualslim <lance@perrybrothersoil.com>
2020-09-23 09:02:10 -0400

    action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "livelyteams_production"
 
-  config.action_mailer.raise_delivery_errors = true
+ config.action_mailer.raise_delivery_errors = false
 
  config.action_mailer.perform_caching = false
 
