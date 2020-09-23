518d763cc5c16a65fd0f81b4dd07d5eea00135b3
unusualslim <lance@perrybrothersoil.com>
2020-09-23 14:21:16 -0400

    more config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :amazon
-  ActiveStorage::Service.url_expires_in = 3.hours
+  #ActiveStorage::Service.url_expires_in = 3.hours
 
 
   # Mount Action Cable outside main process or domain.
