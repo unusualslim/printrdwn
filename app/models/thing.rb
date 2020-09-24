9bff4149d896697763dc6aaeb38881cf15d298ec
unusualslim <lance@perrybrothersoil.com>
2020-09-24 08:45:57 -0400

    adfasdf

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -1,10 +1,10 @@
 class Thing < ApplicationRecord
   has_many_attached :files
 
-  whitelist = ['image/png', 'image/jpeg', 'image/gif', 'video/mp4', 'application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'video/x-matroska', 'video/webm' ]
+#  whitelist = ['image/png', 'image/jpeg', 'image/gif', 'video/mp4', 'application/pdf', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/vnd.ms-excel', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'application/vnd.openxmlformats-officedocument.presentationml.presentation', 'video/x-matroska', 'video/webm' ]
 
 
-  #validates :files, presence: true
-  validates :files, presence: true, blob: { content_type: whitelist}
+  validates :files, presence: true
+#  validates :files, presence: true, blob: { content_type: whitelist}
 #  belongs_to :team
 end
