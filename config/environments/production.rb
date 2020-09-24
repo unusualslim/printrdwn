0aa4c8b0683dd660af6e57c28b2f0a67272c25d9
unusualslim <lance@perrybrothersoil.com>
2020-09-24 08:28:17 -0400

    configa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
  config.action_mailer.perform_deliveries = true
 
- config.action_mailer.delivery_method = :sendmail
+ config.action_mailer.delivery_method = :smtp
 
  #SMTP Server settings
  config.action_mailer.smtp_settings = {
